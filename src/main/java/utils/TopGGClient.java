817bd92300430a7b6219cd6cb6d3e1c0c83cd71f
SniperNoob95 <jmyerson9@yahoo.com>
2022-05-08 22:12:17 -0400

    Fixed top.gg stats update.

--- src/main/java/utils/TopGGClient.java
+++ src/main/java/utils/TopGGClient.java
@@ -1,26 +1,27 @@
 package utils;
 
 import bot.NASABot;
-import org.discordbots.api.client.DiscordBotListAPI;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import org.json.JSONObject;
+
 import java.util.ResourceBundle;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class TopGGClient {
 
-    DiscordBotListAPI discordBotListAPI;
+    private final static String url = "https://top.gg/api/bots/748775876077813881";
+    private static String token;
+    private final OkHttpClient httpClient = new OkHttpClient().newBuilder().build();
 
     public TopGGClient() {
         ResourceBundle resourceBundle = ResourceBundle.getBundle("config");
 
         try {
-            String authKey = resourceBundle.getString("TopGGKey");
-            discordBotListAPI = new DiscordBotListAPI.Builder()
-                    .token(authKey)
-                    .botId(NASABot.jda.getSelfUser().getId())
-                    .build();
-
-            discordBotListAPI.getBot(NASABot.jda.getSelfUser().getId());
+            token = resourceBundle.getString("TopGGKey");
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("Cannot load top.gg client.");
@@ -32,11 +33,22 @@ public class TopGGClient {
         timer.scheduleAtFixedRate(timerTask, 0, 5*1000);
     }
 
+    private static void setStats(OkHttpClient httpClient, int serverCount) {
+        try {
+            RequestBody requestBody = RequestBody.create(MediaType.parse("application/json"), new JSONObject().put("server_count", serverCount).toString());
+            Request request = new Request.Builder().url(url + "/stats").method("POST", requestBody).addHeader("Authorization", "Bearer " + token).build();
+            httpClient.newCall(request).execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Failed to update status.");
+        }
+    }
+
     private class TopGGTimerTask extends TimerTask {
 
         @Override
         public void run() {
-            discordBotListAPI.setStats(NASABot.jda.getGuilds().size());
+            setStats(httpClient, NASABot.jda.getGuilds().size());
         }
     }
 }
